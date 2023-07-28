1358bf857c8d26c2a8eb5386c13f60c0069c1259
miaoxiaowen <8kXnKjzQfZBo1HJY>
2023-07-28 11:32:44 +0800

    update

--- src/com/mxw/leetcode/Demo/Demo1.java
+++ src/com/mxw/leetcode/Demo/Demo1.java
@@ -5,5 +5,20 @@ import java.util.*;
 
 public class Demo1 {
 
+    private int[] preSum;
+
+
+    public Demo1(int[] nums) {
+
+        preSum =new int[nums.length+1];
+
+        for (int i = 1; i < preSum.length; i++) {
+            preSum[i] = preSum[i-1]+nums[i-1];
+        }
+    }
+
+    public int sumRange(int left , int right) {
+        return preSum[right+1]-preSum[left];
+    }
 
 }
--- src/com/mxw/leetcode/前缀和数组/A2二维区域和检索矩阵不可变.java
+++ src/com/mxw/leetcode/前缀和数组/A2二维区域和检索矩阵不可变.java
@@ -4,14 +4,32 @@ public class A2二维区域和检索矩阵不可变 {
 
     /**
      给定一个二维矩阵 matrix，以下类型的多个请求：
+
      计算其子矩形范围内元素的总和，该子矩阵的 左上角 为 (row1,col1) ，右下角 为 (row2,col2) 。
+     实现 NumMatrix 类：
+
+     NumMatrix(int[][] matrix)给定整数矩阵 matrix 进行初始化
+     int sumRegion(int row1, int col1, int row2, int col2)返回 左上角 (row1,col1)、右下角(row2,col2) 所描述的子矩阵的元素 总和 。
+
+     输入:
+     ["NumMatrix","sumRegion","sumRegion","sumRegion"]
+     [[[[3,0,1,4,2],[5,6,3,2,1],[1,2,0,1,5],[4,1,0,1,7],[1,0,3,0,5]]],[2,1,4,3],[1,1,2,2],[1,2,2,4]]
+     输出:
+     [null, 8, 11, 12]
+
+     解释:
+     NumMatrix numMatrix = new NumMatrix([[3,0,1,4,2],[5,6,3,2,1],[1,2,0,1,5],[4,1,0,1,7],[1,0,3,0,5]]);
+     numMatrix.sumRegion(2, 1, 4, 3); // return 8 (红色矩形框的元素总和)
+     numMatrix.sumRegion(1, 1, 2, 2); // return 11 (绿色矩形框的元素总和)
+     numMatrix.sumRegion(1, 2, 2, 4); // return 12 (蓝色矩形框的元素总和)
+
      */
 
     // 定义：preSum[i][j] 记录 matrix 中子矩阵 [0, 0, i-1, j-1] 的元素和
     private int[][] preSum;
 
-    public A2二维区域和检索矩阵不可变(int[][] matrix) {
-        int m = matrix.length, n = matrix[0].length;
+    public A2二维区域和检索矩阵不可变(int[][] nums) {
+        int m = nums.length, n = nums[0].length;
         if (m == 0 || n == 0) {
             return;
         }
@@ -20,7 +38,7 @@ public class A2二维区域和检索矩阵不可变 {
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 // 计算每个矩阵 [0, 0, i, j] 的元素和
-                preSum[i][j] = preSum[i-1][j] + preSum[i][j-1] + matrix[i - 1][j - 1] - preSum[i-1][j-1];
+                preSum[i][j] = preSum[i-1][j] + preSum[i][j-1] - preSum[i-1][j-1] + nums[i - 1][j - 1];
             }
         }
     }
@@ -28,6 +46,6 @@ public class A2二维区域和检索矩阵不可变 {
     // 计算子矩阵 [x1, y1, x2, y2] 的元素和
     public int sumRegion(int x1, int y1, int x2, int y2) {
         // 目标矩阵之和由四个相邻矩阵运算获得
-        return preSum[x2+1][y2+1] - preSum[x1][y2+1] - preSum[x2+1][y1] + preSum[x1][y1];
+        return preSum[x2+1][y2+1] + preSum[x1][y1] - preSum[x1][y2+1] - preSum[x2+1][y1];
     }
 }
